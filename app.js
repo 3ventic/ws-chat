7d964a95a10e1c139a682a5c2b5f701b71c84d46
3ventic <3ventic@gmail.com>
2015-07-12 04:03:38 +0300

    added styles

--- app.js
+++ app.js
@@ -7,17 +7,9 @@ Auth
 
 function Auth()
 {
-    var username = "justinfan1";
+    var username;
     var token;
     var hash = window.location.hash;
-
-    this.data = function ()
-    {
-        return {
-            username: this.username,
-            token: this.token
-        }
-    }
 
     this.apiRequest = function (endpoint, data, callback)
     {
@@ -35,8 +27,17 @@ function Auth()
         return match ? match[1] : null;
     }
 
+    this.authorized = function ()
+    {
+        $('#auth').addClass('hidden');
+        $('#app').removeClass('hidden');
+        window.history.pushState(null, null, window.location.search.length > 0 ? window.location.search : "?");
+        chat.init();
+    }
+
     this.authorize = function ()
     {
+        var self = this;
         if (!(this.token = localStorage.getItem('token')))
         {
             if (this.token = this.hashMatch(/access_token=(\w+)/))
@@ -50,13 +51,19 @@ function Auth()
         }
         this.apiRequest('kraken/', { oauth_token: this.token }, function (data)
         {
-            if (data.responseJSON.token.valid && data.responseJSON.token.authorization.scopes.indexOf('chat_login') >= 0)
-            {
-                this.authorized();
-            }
+            self.tokenCheckCallback(data);
         });
 
         return true;
+    }
+
+    this.tokenCheckCallback = function (data)
+    {
+        if (data.responseJSON.token.valid && data.responseJSON.token.authorization.scopes.indexOf('chat_login') >= 0)
+        {
+            this.username = data.responseJSON.token.user_name;
+            this.authorized();
+        }
     }
 
     this.redirectToTwitchAuth = function ()
@@ -65,13 +72,6 @@ function Auth()
             + Auth.clientId + "&redirect_uri="
             + encodeURIComponent(window.location.href.substring(0, window.location.href.lastIndexOf('/') + 1)) +
             "&scope=chat_login";
-    }
-
-    this.authorized = function ()
-    {
-        $('#auth').addClass('hidden');
-        $('#app').removeClass('hidden');
-        chat.init();
     }
 }
 
@@ -96,10 +96,17 @@ Chat
 
 function Chat()
 {
+    var connection;
+    var channel;
+
     this.init = function ()
     {
-
+        if (typeof this.channel === "undefined")
+            this.channel = ("channel" in QueryString) ? QueryString["channel"] : window.prompt("Channel?");
+        connection = new Connection();
     }
+
+
 }
 
 /*
@@ -111,9 +118,35 @@ Connection
 function Connection()
 {
     var ws = new WebSocket('wss://i.3v.fi:8016/');
+    var anonymous = ("anonymous" in QueryString);
+    var self = this;
 
     ws.onopen = function ()
     {
-        ws.send('CHANNEL ' + chat.channel);
+        ws.send('CHANNEL ' + (anonymous ? "!" : "") + chat.channel);
+    }
+
+    ws.onmessage = function (event)
+    {
+        self.onWsMessage(event);
+    }
+
+    this.onWsMessage = function (event)
+    {
+        var data = parseMessage(event.data);
+
+        switch (data.command)
+        {
+            case "RELAYAUTH":
+                ws.send('CAP REQ :twitch.tv/tags twitch.tv/commands');
+                ws.send('PASS oauth:' + auth.token);
+                ws.send('NICK ' + auth.username);
+                if (!anonymous) ws.send('JOIN #' + chat.channel);
+                break;
+            case "PING":
+                ws.send('PONG');
+                break;
+
+        }
     }
 }
